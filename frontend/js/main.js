7c2d4817c8b2224560432a3b766a1a5368e63d09
pikitosdoritos <fastovetsn@gmail.com>
2026-05-11 17:04:45 +0300

    Refactor updateOutput function to build and render HTML page with CSS and JS integration

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -3,8 +3,36 @@ const cssArea = document.getElementById('css_area');
 const jsArea = document.getElementById('js_area');
 const outputArea = document.getElementById('output_area');
 
-htmlArea.oninput = updateOutput;
+htmlArea.oninput = cssArea.oninput = jsArea.oninput = updateOutput;
+
+function renderPreview(html) {
+    outputArea.srcdoc = html;    
+}
+
+function buildPage() {
+    let html = '';
+
+    if (htmlArea.value.match(/<\/?(html|head|body)[^>]*>/gmi)?.length != 6) {
+        html = `
+        <html>
+            <head>
+                <style>${cssArea.value}</style>
+            </head>
+            <body>
+                ${htmlArea.value}
+                <script>${jsArea.value}</script>
+            </body>
+        </html>
+        `;
+    } else {
+        html = htmlArea.value;
+        html = html.replace(/<\/head>/, `<style>${cssArea.value}</style></head>`);
+        html = html.replace(/<\/body>/, `<script>${jsArea.value}</script></body>`);
+    }
+
+    return html;
+}
 
 function updateOutput() {
-    outputArea.srcdoc = htmlArea.value;
+    renderPreview(buildPage());
 }

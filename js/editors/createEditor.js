2093c2f9595dadb9b733849c50926387910e57d8
pikitosdoritos <fastovetsn@gmail.com>
2026-05-13 15:24:00 +0300

    Update Monaco Editor import and enable automatic layout for all editors

--- js/editors/createEditor.js
+++ js/editors/createEditor.js
@@ -1,27 +1,26 @@
-import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
+import * as monaco from 'monaco-editor';
 
 const htmlContainer = document.getElementById('html_editor');
 const cssContainer = document.getElementById('css_editor');
 const jsContainer = document.getElementById('js_editor');
 
-
 export const htmlEditor = monaco.editor.create(htmlContainer, {
     value: '<h1>Hello</h1>',
     language: 'html',
     theme: 'vs-dark',
-    automaticLayout: false
+    automaticLayout: true
 });
 
 export const cssEditor = monaco.editor.create(cssContainer, {
     value: 'h1 { color: red; }',
     language: 'css',
     theme: 'vs-dark',
-    automaticLayout: false
+    automaticLayout: true
 });
 
 export const jsEditor = monaco.editor.create(jsContainer, {
     value: 'console.log("Hello");',
     language: 'javascript',
     theme: 'vs-dark',
-    automaticLayout: false
+    automaticLayout: true
 });
